c6878694a3323c13af7e9c89b3633a2589402412
HinxVietti <hinxvietti@gmail.com>
2018-12-17 18:05:01 +0800

    add demo files

---
+++ DemoApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HinxCor.Drawing;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        private static void DemoMethod5d3f()
+        {
+            Console.WriteLine("writeImage");
+            var string  = Console.ReadLine;
+        }
+
+        private static void DemoApp23ef()
+        {
+            Console.WriteLine("This is an console demo app, any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
