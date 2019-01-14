a6b38a2b987495c6c663bdfeabb2a16a3bf15edc
HinxVietti <hinxvietti@gmail.com>
2019-01-14 16:48:09 +0800

    修改了生成参数

--- HinxCor.Rendering/Properties/AssemblyInfo.cs
+++ HinxCor.Rendering/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("HinxCor.Rendering")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Hinx.Vietti 公开工具，C# dot Net绘制渲染工具 @HinxVietti GitHUB https://github.com/HinxVietti/HinxCor")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HinxCor.Rendering")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("HinxCor MIT Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.3")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
